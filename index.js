b333742fc9a1b0b0775e89e47919c41564aba10a
Shifra Wagshall <shifrawagshall@gmail.com>
2022-02-12 21:05:29 -0500

    Added questions, and functions needed for index.

--- index.js
+++ index.js
@@ -1,1 +1,90 @@
 
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown=require('./generateMarkdown');
+const util= require('util');
+
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// TODO: Create an array of questions for user input
+inquirer
+.prompt[
+// const questions = [];
+      {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+      },
+      {
+        type: "input",
+        message: "What is your project name?",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "Write a short description of your project.",
+        name: "description"
+      },
+      {
+        type: "checkbox",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+          "MIT",
+          "APACHE 2.0",
+          "BSD 3",
+          "Other",
+          "None"
+        ]
+      },
+      {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "instillation"
+      },
+      {
+        type: "input",
+        message: "Please provide the instructions on usage of the application.",
+        name: "usage"
+      },
+      {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "test"
+      },
+      {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contribution"
+      }
+    ]
+  
+  
+    const askUser = () => {
+        return inquirer
+            .prompt();
+    } 
+  
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return writeFileAsync(fileName, data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+   const info= askUser();
+
+    then((data) => {
+
+        const contentFile=generateMarkdown(info)
+        console.log(data)
+          return writeToFile("Test.md", contentFile);
+
+    })
+}
+
+// Function call to initialize app
+init();
